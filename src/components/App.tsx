d2e292120a0762ee2faa6bd8d3b3386247c6f98a
Sudhir Unde <sudhirunde113@gmail.com>
2022-06-05 14:17:48 +0530

    file changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,8 +5,6 @@ const reactLogo = require("./../assets/img/react_logo.svg");
 
 const App = () => (
   <div className="app">
-    <h1>Hello World!</h1>
-    <p>Foo to the barz</p>
     <img src={reactLogo.default} height="480" />
   </div>
 );
