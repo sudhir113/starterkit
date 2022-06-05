a994eacfce00acf0a2d8ac7e4fb98e3371f6ce5b
Sudhir Unde <sudhirunde113@gmail.com>
2022-06-05 14:30:33 +0530

    added redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import App from "./components/App";
+import React, { lazy } from 'react';
 import { createRoot } from "react-dom/client";
 
+const App = lazy(() => import('./components/App'));
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(<App />);
