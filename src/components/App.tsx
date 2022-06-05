9eea9e1b0a291fc811125c2ca7f15a1b80567945
Sudhir Unde <sudhirunde113@gmail.com>
2022-06-05 19:27:44 +0530

    added home component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
-import * as React from "react";
+import {lazy} from "react";
+import { BrowserRouter ,Route,Routes } from "react-router-dom";
 import "./../assets/scss/App.scss";
 
 const reactLogo = require("./../assets/img/react_logo.svg");
+const Home = lazy(() => import('./home'));
 
 const App = () => (
-  <div className="app">
-    <h1>hello world app</h1>
-  </div>
+  <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+  </Routes>
+  </BrowserRouter>
 );
 
 export default App;
