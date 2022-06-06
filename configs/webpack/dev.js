10266cd465e8f5815d2c63faa20aa82f038c9c73
Sudhir Unde <sudhirunde113@gmail.com>
2022-06-06 16:29:50 +0530

    node env added

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -10,5 +10,10 @@ module.exports = merge(commonConfig, {
     historyApiFallback: true, // fixes error 404-ish errors when using react router :see this SO question: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url
   },
   devtool: "cheap-module-source-map",
-  plugins: [new ReactRefreshPlugin()],
+  plugins: [new ReactRefreshPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.MY_ENV': JSON.stringify(process.env.MY_ENV)
+    })
+  ],
 });
