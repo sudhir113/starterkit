b9b77d55a6a1feb6fcac5db30a774421caa869c9
Sudhir Unde <sudhirunde113@gmail.com>
2022-06-07 15:20:18 +0530

    added date picker

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import {useState} from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Home =()=>{
+    const [startDate, setStartDate] = useState(new Date());
     return(
         <>
             <h1>Home</h1>
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} showYearPicker />
         </>
     )
 }
